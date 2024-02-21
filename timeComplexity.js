fedf0e84894add6b27e33727fa9107dd2d76617a
belaeat <omyhossain@gmail.com>
2024-02-21 20:52:21 +0600

    practicing time and space complexity

---
+++ timeComplexity.js
@@ -0,0 +1,9 @@
+/**     TIME COMPLEXITY 
+ * 
+ * Assignment Operation (=)
+ * Comparison Operation ( a > b, ==, === etc)
+ * Mathematical Operation (a+b)
+ * Calling a Function ()
+ * Executing Time of a Function (time required for complete the inner workflow)
+ * 
+ */
